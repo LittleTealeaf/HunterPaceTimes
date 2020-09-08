2d8a07b349b4beb64e96cda76d0e087e9edec504
LittleTealeaf <shintaomonk@gmail.com>
2020-09-08 14:44:19 -0400

    Started work on a scheduler

--- src/Settings/SettingsUpdater.java
+++ src/Settings/SettingsUpdater.java
@@ -1,14 +1,7 @@
 package Settings;
 
 
-import java.util.Date;
-
 public class SettingsUpdater {
-
-	private static boolean needsUpdate;
-	private static Date timeOfLastUpdate;
-	private static boolean updateWaiting;
-	private static int waitDelay;
 
 
 	public static void requestSave() {
